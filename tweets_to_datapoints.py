8ab36ab9d38992dcfce1d0bc4ad5370cc21060bb
Weuler Borges <weulerfilho@outlook.com>
2019-11-14 17:36:56 -0300

    Added code to transform tweet data into datapoints

---
+++ tweets_to_datapoints.py
@@ -0,0 +1,122 @@
+from influxdb import InfluxDBClient
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import math
+import datetime as dt
+import re
+
+def clean(text):
+    '''
+    Removes twitter handles, RT handles, URLs and special characters
+    '''
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r'@[\w]*', "", text)
+    text = re.sub(r'RT @[\w]*', "", text)  
+    text = re.sub(r'[^a-zA-Z#!:)(=)]', ' ', text)
+    text = re.sub(' +', ' ', text)
+
+    return text
+
+# Para cada hora:
+
+# - Pega o conjunto de tweets (OK)
+# - Limpa os textos (OK)
+# - Analisa os sentimentos (OK)
+# - Calcula features dos tweets (OK)
+# - Busca dados sobre bitcoins naquela hora
+# - Insere datapoint (vetor de feature)
+
+# - Calcula a função Z(t) objetivo para aquele datapoint (baseado no paper entropy-21)
+
+# Objetivo: 2160 datapoints
+
+db_client = InfluxDBClient('localhost', 8086, 'root', 'root', 'bitcoin_tweets')
+
+def to_ns(ts):
+    return int(ts) * 1000000000
+
+def get_tweets(time):
+    '''
+    Get 1 hour of tweets in InfluxDB from a given datetime.
+    '''
+    current_ts = to_ns(time.timestamp())
+    next_ts = to_ns(time.timestamp() + 3600)
+
+    results = db_client.query(f'SELECT * FROM tweet WHERE time >= {current_ts} and time < {next_ts}')
+
+    data = []
+
+    for r in results.get_points():
+        data.append(r)
+
+    return data
+
+def get_hourly_clean_tweets(time):
+    '''
+    Get 1 hour of clean tweets starting from the given {time}
+    '''
+    data = get_tweets(time)
+    
+    return [ clean(tweet['text']) for tweet in data ]
+
+def analyze_sentiment(text):
+    '''
+    Given a clean piece of text, analyze VaderSentiment scores for it
+    '''
+    analyzer = SentimentIntensityAnalyzer()
+    return analyzer.polarity_scores(text)
+
+def get_hourly_tweet_features(time):
+    '''
+    Given a time, analyze one hour of tweets starting from that time and calculate features.
+    '''
+    tweets = get_hourly_clean_tweets(time)
+    n = len(tweets)
+
+    sum_neu = 0
+    sum_neg = 0
+    sum_norm = 0
+    sum_pos = 0
+
+    for tweet in tweets:
+        v = analyze_sentiment(tweet)
+
+        sum_neu += v['neu']
+        sum_neg += v['neg']
+        sum_norm += v['compound']
+        sum_pos += v['pos']
+
+    # [ neu, norm, neg, pos, pol ]
+    return [sum_neu/n , sum_neg/n, sum_norm/n, sum_pos/n, math.sqrt( sum_pos/n * sum_neg/n )]
+
+def get_all_tweets(start_time, data_points=24):
+    '''
+    Get {data_points} hourly tweets starting at {start_time}.
+    '''
+    all_data = []
+
+    for date in (start_time + dt.timedelta(n) for n in range(data_points)):
+
+        data = get_tweets(date)
+        print(len(data))
+        all_data.append(data)
+
+    return all_data
+
+start_date = dt.datetime(2019, 1, 15,tzinfo=dt.timezone.utc)
+
+# print(start_date.timestamp())
+# print((start_date + dt.timedelta(hours=1)).timestamp())
+
+# for d in data:
+#     print('-'*25)
+#     print(d['text'])
+#     print('-'*25)
+
+# print(len(get_all_tweets(start_date)))
+# for t in get_hourly_clean_tweets(start_date):
+#     print("-"*20)
+#     print(t)
+#     print("-"*20)
+
+# print(analyze_sentiment("Hello, I'm very happy today with Bitcoin! :)"))
+print(get_hourly_tweet_features(start_date))
