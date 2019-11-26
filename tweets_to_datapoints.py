8cd9ea22d1280ee0644c74b4bbaa1020a98aadce
Weuler Borges <weulerfilho@outlook.com>
2019-11-26 13:43:58 -0300

    Added the option to aggregate data by day.

--- tweets_to_datapoints.py
+++ tweets_to_datapoints.py
@@ -30,8 +30,10 @@ def clean(text):
 # - Busca dados sobre bitcoins naquela hora (OK)
 # - Calcula datapoint (vetor de feature) (OK)
 # - Calcula a função Z(t) objetivo para aquele datapoint (OK)
+# - Guarda tudo com Pickle (OK)
 
-# - Guarda tudo com Pickle
+# ------------------------
+# Teste: coletar market data no mesmo periodo do paper e tentar obter resultados iguais
 
 # Objetivo: 2160 datapoints
 
@@ -43,7 +45,7 @@ analyzer = SentimentIntensityAnalyzer()
 def to_ns(ts):
     return int(ts) * 1000000000
 
-def get_hourly_target_function(time):
+def get_target_function(time, h=1):
     '''
     Get Z(t) for the hour starting at {time}.
 
@@ -52,36 +54,51 @@ def get_hourly_target_function(time):
         - else, Z(t) = -1
     '''
 
-    current_closing = get_hourly_btc_data(time)['close']
-    next_closing = get_hourly_btc_data(time + dt.timedelta(hours=1))['close']
+    current_closing = get_btc_data(time, h)['close']
+    next_closing = get_btc_data(time + dt.timedelta(hours=h), h)['close']
 
     return 1 if next_closing >= current_closing else -1
 
-def get_hourly_btc_data(time):
+def get_btc_data(time, hours=1):
     '''
     Get stats for starting at {time}
 
     --- Note that you need to use a time/timestamp that is actually present in the database ---
     '''
-    results = db_client.query(f'SELECT * FROM bitcoin WHERE time = {to_ns(time.timestamp())}')
+    next_time = time + dt.timedelta(hours=hours)
+    results = db_client.query(f'SELECT * FROM bitcoin WHERE time >= {to_ns(time.timestamp())} and time < {to_ns(next_time.timestamp())}')
+    results = list(results.get_points())
 
-    return next(results.get_points())
+    print(time, hours)
+    
+    # Aggregate results if greater than 1
+    if len(results) > 1:
+        data = {}
+        data['close'] = results[-1]['close']
+        data['open'] = results[0]['open']
+        data['high'] = max(results, key= lambda r: r['high'])['high']
+        data['low'] = min(results, key= lambda r: r['low'])['low']
+        data['volumeto'] = sum(r['volumeto'] for r in results)
 
-def get_hourly_btc_features(time):
+        return data
+
+    return results[0]
+
+def get_btc_features(time, hours=1):
     '''
     Calculate hourly BTC features for {time}
     '''
 
     # [ close, high, low, open, volumeto ]
-    data = get_hourly_btc_data(time)
+    data = get_btc_data(time, hours)
     return [ data['close'], data['high'], data['low'], data['open'], data['volumeto'] ]
 
-def get_tweets(time):
+def get_tweets(time, hours=1):
     '''
-    Get 1 hour of tweets in InfluxDB from a given datetime.
+    Get {hours} of tweets in InfluxDB from a given datetime.
     '''
     current_ts = to_ns(time.timestamp())
-    next_ts = to_ns(time.timestamp() + 3600)
+    next_ts = to_ns(time.timestamp() + (3600 * hours))
 
     results = db_client.query(f'SELECT * FROM tweet WHERE time >= {current_ts} and time < {next_ts}')
 
@@ -92,11 +109,11 @@ def get_tweets(time):
 
     return data
 
-def get_hourly_clean_tweets(time):
+def get_clean_tweets(time, hours=1):
     '''
-    Get 1 hour of clean tweets starting from the given {time}
+    Get {hours} of clean tweets starting from the given {time}
     '''
-    data = get_tweets(time)
+    data = get_tweets(time, hours)
     
     return [ clean(tweet['text']) for tweet in data ]
 
@@ -106,11 +123,11 @@ def analyze_sentiment(text):
     '''
     return analyzer.polarity_scores(text)
 
-def get_hourly_tweet_features(time):
+def get_tweet_features(time, hours=1):
     '''
-    Given a time, analyze one hour of tweets starting from that time and calculate features.
+    Given a time, analyze {hours} of tweets starting from that time and calculate features.
     '''
-    tweets = get_hourly_clean_tweets(time)
+    tweets = get_clean_tweets(time, hours)
     n = len(tweets)
 
     if n == 0:
@@ -132,13 +149,17 @@ def get_hourly_tweet_features(time):
     # [ neu, norm, neg, pos, pol ]
     return [sum_neu/n , sum_neg/n, sum_norm/n, sum_pos/n, math.sqrt( sum_pos/n * sum_neg/n )]
 
-def get_hourly_features(time):
-    return get_hourly_btc_features(time) + get_hourly_tweet_features(time)
+def get_features(time, hours=1):
+    return get_btc_features(time, hours) + get_tweet_features(time, hours)
 
-def get_all(start_time, data_points=24):
+def get_all(start_time, data_points=24, daily=False):
     '''
     Get twitter, market and twitter + market {data_points} hourly datapoints starting at {start_time}.
     '''
+    period = 1
+    if daily:
+        period = 24
+
     twitter_vt = []
     twitter_zt = []
 
@@ -148,17 +169,17 @@ def get_all(start_time, data_points=24):
     market_vt = []    
     market_zt = []
 
-    for date in (start_time + dt.timedelta(hours=n) for n in range(data_points)):
+    for date in (start_time + dt.timedelta(hours=n*period) for n in range(data_points)):
         print(f"---- Started {date} ----")
 
-        target = get_hourly_target_function(date)
+        target = get_target_function(date, period)
 
-        market_features = get_hourly_btc_features(date)
+        market_features = get_btc_features(date, period)
         market_vt.append(market_features)
         market_zt.append(target)
         
         try:
-            twitter_features = get_hourly_tweet_features(date)
+            twitter_features = get_tweet_features(date, period)
 
             twitter_vt.append(twitter_features)
             twitter_zt.append(target)
@@ -175,16 +196,17 @@ start_date = dt.datetime(2019, 1, 1, hour=0,tzinfo=dt.timezone.utc)
 # cProfile.run('get_all(start_date, data_points=3)')
 
 # Save datapoints as a tuple
-datapoints = get_all(start_date, data_points=2160)
+datapoints = get_all(start_date, data_points=90, daily=True)
 
 twitter_data = (datapoints[0], datapoints[1])
 market_data = (datapoints[2], datapoints[3])
 mixed_data = (datapoints[4], datapoints[5])
 
-with open('twitter_datapoints.pickle', 'wb') as f:
+with open('daily_twitter_datapoints.pickle', 'wb') as f:
     pickle.dump(twitter_data, f)
-with open('market_datapoints.pickle', 'wb') as f:
+with open('daily_market_datapoints.pickle', 'wb') as f:
     pickle.dump(market_data, f)
-with open('mixed_datapoints.pickle', 'wb') as f:
+with open('daily_mixed_datapoints.pickle', 'wb') as f:
     pickle.dump(mixed_data, f)
 
+# print(get_btc_data(start_date, hours=24))
